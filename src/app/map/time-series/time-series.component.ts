8cc9e35ef063abe2baaf2957469b7d6e6367b079
HugoQN <qiaonanh@uci.edu>
2019-08-12 16:34:57 -0700

    clicking in time serices updates current time in time service

--- src/app/map/time-series/time-series.component.ts
+++ src/app/map/time-series/time-series.component.ts
@@ -1,9 +1,11 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import * as $ from 'jquery';
 import {MapService} from '../../services/map-service/map.service';
+import {TimeService} from '../../services/time/time.service';
 import {Tweet} from '../../models/tweet.model';
 
 declare var require: any;
+const Highcharts = require('highcharts');
 
 @Component({
     selector: 'app-time-series',
@@ -13,16 +15,20 @@ declare var require: any;
 export class TimeSeriesComponent implements OnInit {
 
     @Output() timeRangeChange = new EventEmitter();
+    private hasPlotBand = false;
 
-    constructor(private mapService: MapService) {
+    constructor(private mapService: MapService, private timeService: TimeService) {
     }
 
     ngOnInit() {
-        this.mapService.getFireTweetData().subscribe(this.drawTimeSeries);
+        this.mapService.getFireTweetData().subscribe(data => {
+            this.drawTimeSeries(data);
+        });
+
     }
 
     // Draw time series
-    drawTimeSeries = (tweets: Tweet[]) => {
+    public drawTimeSeries = (tweets: Tweet[]) => {
         const chartData = [];
         const dailyCount = {};
 
@@ -34,14 +40,12 @@ export class TimeSeriesComponent implements OnInit {
                 dailyCount[createAt] = 1;
             }
         }
-
-
         // time bar
         Object.keys(dailyCount).sort().forEach(key => {
             chartData.push([new Date(key).getTime(), dailyCount[key]]);
         });
 
-        require('highcharts').chart('timebar-container', {
+        const timeseries = Highcharts.chart('timebar-container', {
             chart: {
                 type: 'line',
                 zoomType: 'x',
@@ -59,7 +63,38 @@ export class TimeSeriesComponent implements OnInit {
                             timebarEnd = event.target.axes[0].dataMax;
                         }
                         $(window).trigger('timeRangeChange', {timebarStart, timebarEnd});
-                    }
+                    },
+                    // TODO: time can be stored in millesecond, better performance
+                    click: (event) => {
+                        const halfUnit = 86400000 / 2;
+                        let dateInMs = event.xAxis[0].value - event.xAxis[0].value % halfUnit;
+                        dateInMs += dateInMs % (halfUnit * 2);
+                        const dateSelectedInISO = new Date(dateInMs).toISOString();
+                        if (!this.hasPlotBand) {
+                            timeseries.xAxis[0].addPlotBand({
+                                from: dateInMs - halfUnit,
+                                to: dateInMs + halfUnit,
+                                color: '#d88040',
+                                id: 'plotBand'
+                            });
+                            this.hasPlotBand = true;
+                            this.timeService.setCurrentDate(dateSelectedInISO);
+                        } else if (this.ISOToYMD(dateSelectedInISO) !== this.ISOToYMD(this.timeService.getCurrentDate())) {
+                            timeseries.xAxis[0].removePlotBand('plotBand');
+                            timeseries.xAxis[0].addPlotBand({
+                                from: dateInMs - halfUnit,
+                                to: dateInMs + halfUnit,
+                                color: '#d88040',
+                                id: 'plotBand'
+                            });
+                            this.hasPlotBand = true;
+                            this.timeService.setCurrentDate(dateSelectedInISO);
+                        } else {
+                            timeseries.xAxis[0].removePlotBand('plotBand');
+                            this.hasPlotBand = false;
+                            this.timeService.setCurrentDate(null);
+                        }
+                    },
                 }
             },
             title: {
@@ -69,7 +104,8 @@ export class TimeSeriesComponent implements OnInit {
                 }
             },
             xAxis: {
-                type: 'datetime'
+                type: 'datetime',
+                crosshair: true,
             },
             series: [{
                 data: chartData,
@@ -78,6 +114,13 @@ export class TimeSeriesComponent implements OnInit {
             }]
 
         });
-    };
+    }
+
+    ISOToYMD(fullISOString) {
+        return fullISOString.substring(0, 10);
+    }
+    YMDToISO(YMD) {
+        return new Date(YMD).toISOString();
+    }
 
 }
